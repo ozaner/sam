47d38539978969985704f2e9d25e288b46e6d5ab
Fernando Nogueira <abert036@uottawa.ca>
2024-04-12 05:40:08 -0400

    Refactor RenderBuffer function to ToWavBuffer in player.es6

--- src/index.d.ts
+++ src/index.d.ts
@@ -18,6 +18,7 @@ declare class SamJs {
   buf32(text: string, phonetic?: boolean): Float32Array | Boolean;
   speak(text: string, phonetic?: boolean): SamJsSpeakPromise;
   download(text: string, phonetic?: boolean): void;
+  wav(text: string, phonetic?: boolean): Uint8Array;
 }
 
 declare module "sam-js" {
